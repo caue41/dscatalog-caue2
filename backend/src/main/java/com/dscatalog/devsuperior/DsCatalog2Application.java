29955eb6d162e206bc93140ecf6fc4563f26c70f
Cauê Gonçalves Santana <caue41@gmail.com>
2022-12-06 15:04:36 -0300

    Backend project created

---
+++ backend/src/main/java/com/dscatalog/devsuperior/DsCatalog2Application.java
@@ -0,0 +1,13 @@
+package com.dscatalog.devsuperior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsCatalog2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsCatalog2Application.class, args);
+	}
+
+}
